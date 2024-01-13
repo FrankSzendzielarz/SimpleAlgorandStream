419b8a717d2343a914053c959096f828b7f46a11
Frank Szendzielarz <frank@franks.technology>
2024-01-13 12:34:10 +0100

    -

---
+++ SimpleAlgorandStream.ClientTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace SimpleAlgorandStream.ClientTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "AlgorandFeed", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "AlgorandFeed",
+                                  routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = System.Text.Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
